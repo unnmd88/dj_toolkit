bf44875dadb88c4a1a3b1a1ab5e55e18459ceadd
unnmd88_h <unnmd88@gmail.com>
2024-12-08 21:33:02 +0300

    Рефакторинг: удалены неиспользуемые методы и атрибуты в js

--- toolkit/static/toolkit/js/compare_groups.js
+++ toolkit/static/toolkit/js/compare_groups.js
@@ -30,8 +30,6 @@ async function set_request_axios(event) {
 
     const content_table_groups = document.querySelector(`#table_groups`).value;
     const content_table_stages = document.querySelector(`#table_stages`).value;
-    console.log('content_table_groups >> ' + content_table_groups);
-    console.log(content_table_groups);
                                                        
     let csrfToken = $("input[name=csrfmiddlewaretoken]").val();
     try {
@@ -83,70 +81,6 @@ calculate.addEventListener('click', set_request_axios);
  /*------------------------------------------------------------------------
 |                        Запись полученых данных в дисплей                 |
 --------------------------------------------------------------------------*/
-
-// Запись в "дисплей" полученного состояния ДК(get request)
-
- /*------------------------------------------------------------------------
-|                               SEARCH HOST                                |
---------------------------------------------------------------------------*/
-
-
-
-async function search_host_get_data(event) {
-
-    console.log('search_host_get_data');
-    
-    const value = event.target.value;
-    
-    let response;
-    try {
-         response = await axios.get(            
-            // `/api/v1/trafficlight-objects/${value}`,
-            ROOT_ROUTE_API_GET_TRAFFIC_LIGHT_OBJECT + value,
-            {
-            headers: {
-                "Authorization": `Token ${TOKEN}`,
-            },
-            },
-
-        );
-        console.log('response.data');
-        console.log(response.data);
-
-
-
-      } catch (error) {
-        if (error.response) { // get response with a status code not in range 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) { // no response
-          console.log(error.request);
-        } else { // Something wrong in setting up the request
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
-        response = error.response;
-
-      } finally {
-        write_data_about_host(response.data);
-        check_valid_data_to_send_req(response.data);
-      }
-    
- }
-
-
-function check_valid_data_to_send_req(data) {
-    if (data.type_controller === CONTROLLERS[0]) {
-        console.log(data);
-        document.querySelector('#get_config').disabled = false;
-    }
-    else {
-        document.querySelector('#get_config').disabled = true;
-    }
-}
-
-
 
 
 
