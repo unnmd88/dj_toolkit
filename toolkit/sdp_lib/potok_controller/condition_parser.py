1f9c9dbf65fe4c99eabe59180a803e2772ddbb30
unnmd88_h <unnmd88@gmail.com>
2024-12-20 02:35:46 +0300

    Реализация новой логики парса строки условие перехода/продления в модуле condition_parser.py

--- toolkit/sdp_lib/potok_controller/condition_parser.py
+++ toolkit/sdp_lib/potok_controller/condition_parser.py
@@ -1,7 +1,164 @@
 import re
 from typing import List, Dict
 
+from rply import LexerGenerator
+
 from .constants import DET_FUNCTIONS, ALLOWED_FUNCTIONS, SG_FUNCTIONS
+
+
+# class ConditionParser:
+#     """
+#     Класс - обрабчик в строки условия перехода/продления
+#     Traffic lights configurator контроллера Поток
+#     """
+#
+#     def __init__(self, condition_string: str):
+#         """
+#         :param condition_string: Строка с условием перехода/продления
+#                                  Traffic lights configurator контроллера Поток
+#         """
+#
+#         self.condition_string = condition_string
+#         self.tokens = None
+#
+#     def create_tokens(self) -> List:
+#         """
+#         Формирует список токенов(функций) из self.condition_string
+#         :return: Список токенов(функций) из self.condition_string
+#         """
+#
+#         tokens = []
+#         data = self.condition_string.split()
+#
+#         for i, fragment in enumerate(data):
+#             token = None
+#             for f_name in ALLOWED_FUNCTIONS:
+#                 if f_name in fragment:
+#                     arg = self._get_arg(f_name, fragment)
+#                     token = self.add_token(f_name, fragment, arg)
+#                     if f_name == 'fctg':
+#                         comparison_operator, val = data.pop(i + 1), data.pop(i + 1)
+#                         while val[-1] == ')':
+#                             if not val:
+#                                 raise ValueError(f'Недопустимое значение функции fctg: {token}')
+#                             val = val[:-1]
+#                         token = self._validate_fctg(token, comparison_operator, val)
+#                     break
+#             if token is None:
+#                 continue
+#
+#             if i > 0 and 'not' in data[i - 1]:
+#                 token = f'not {token}'
+#
+#             tokens.append(token)
+#         self.tokens = tokens
+#         return tokens
+#
+#     def _validate_fctg(self, token: str, operator: str, value: str) -> str:
+#         """
+#         Проверяет валидность оператора и значения для функции fctg.
+#         :param token: fctg по дефолту
+#         :param operator: оператор из функции условия вызова/продления
+#         :param value: значение из функции условия вызова/продления
+#         :return: Токен(функция). Пример: 'fctg(G1) >= 40'
+#         """
+#
+#         if operator not in {'>', '<', '==', '>=', '<='}:
+#             raise TypeError(f'Недопустимый оператор для функции fctg: {operator}')
+#         if not value.isdigit() or value.startswith('0'):
+#             raise ValueError(f'Недопустимое значение функции fctg: {value}')
+#         return f'{token} {operator} {value}'
+#
+#     def add_token(self, f_name: str, fragment: str, name_argument: str) -> str:
+#         """
+#         Генерирует токен(функцию)
+#         :param f_name: имя функции: 'ddr', 'mr', 'fctg' и т.д.
+#         :param fragment: элемент списка, из строки self.condition_string.split()
+#                          '(((ddr(D1)', '(ddr(D2))', 'ddr(D3)', 'fctg >= 40' и т.д.
+#         :param name_argument: Символ аргумента в зависимости от типа функции:
+#                               D если ddr, G или A если mr и т.д.
+#         :return: Токен(функция) вида: 'ddr(D22)', fctg(G1), 'ngp(D21)' и т.д.
+#         """
+#
+#         patterns_regexp = {
+#             'ddr': r'[0-9]{1,3}',
+#             'ddo': r'[0-9]{1,3}',
+#             'ngp': r'[0-9]{1,3}',
+#             'mr': r'[0-9]{1,3}',
+#             'dr': r'[0-9]{1,3}',
+#             'fctg': r'[0-9]{1,3}'
+#         }
+#
+#         pattern = patterns_regexp.get(f_name)
+#         digit_argument = re.findall(pattern, fragment)
+#         if len(digit_argument) > 1 or int(digit_argument[0]) > 999 or digit_argument[0].startswith('0'):
+#             raise ValueError(f'Неверный аргумент функции {f_name}: {"".join(digit_argument)}')
+#         return f'{f_name}({name_argument}{digit_argument[0]})'
+#
+#     def _get_arg(self, f_name: str, fragment: str) -> str:
+#         """
+#         Определяет часть аргумента для переданной функции.
+#         :param f_name: имя токена-функции контроллера Поток: 'ddr', 'mr', 'fctg' и т.д.
+#         :param fragment: Фрагмент, в котором осуществляется поиск(элемент списка, получившегося после
+#                          функции split() на строке условии вызова/продления.
+#                          Например: '((ddr(D25)', 'ddr(D5)', 'mr(G1)', 'and' и т.д.
+#         :return: аргумент для функции: D(для функии дет), G или A для функции sg
+#         """
+#
+#         if f_name in DET_FUNCTIONS:
+#             if 'D' not in fragment:
+#                 raise ValueError(f'Неверный аргумент одной из фунций {f_name}')
+#             arg = 'D'
+#         elif f_name in SG_FUNCTIONS:
+#             if 'G' in fragment:
+#                 arg = 'G'
+#             elif 'A' in fragment:
+#                 arg = 'A'
+#             else:
+#                 raise ValueError(f'Неверный аргумент одной из фунций {f_name}')
+#         else:
+#             raise TypeError(f'Передана некорректная функция {f_name}')
+#         return arg
+#
+#     @classmethod
+#     def get_condition_string_with_vals_instead_func(cls, string: str) -> str:
+#         """
+#         Заменяет "or" и "and" в строке на "+" и "*" соответственно.
+#         :param string: строка, в которой требуется заменить символы.
+#         :return: строка с заменёнными символами.
+#         """
+#
+#         return cls.replace_chars(replace_data={'and': '*', 'or': '+'}, string=string)
+#
+#     @classmethod
+#     def replace_chars(cls, replace_data: Dict[str, str], string: str):
+#         """
+#         Заменяет символы в строке.
+#         :param string: строка, в которой требуется заменить символы.
+#         :param replace_data: k: символы, которые требуется заемить, v: символы, на которые требуется заемить
+#         :return: строка с заменёнными символами.
+#         """
+#
+#         for pattern, replacement in replace_data.items():
+#             string = string.replace(pattern, replacement)
+#         return string
+
+class Lexer:
+
+    @classmethod
+    def get_lexer(cls):
+        lg = LexerGenerator()
+        lg.add("ddr", r'ddr\(D\d{1,3}\)')
+        lg.add("ddo", r'ddo\(D\d{1,3}\)')
+        lg.add("ngp", r'ngp\(D\d{1,3}\)')
+        lg.add("fctg", r'fctg\(G\d+\)\s*([<>]=?|==)\s*(\d+)')
+        lg.add('mr', r'mr\(G\d{1,3}\)')
+        lg.ignore(r'not|or|and|[\s+\(\)]')
+        return lg
+
+
+l_ = Lexer.get_lexer()
+lexer = l_.build()
 
 
 class ConditionParser:
@@ -25,113 +182,8 @@ class ConditionParser:
         :return: Список токенов(функций) из self.condition_string
         """
 
-        tokens = []
-        data = self.condition_string.split()
+        self.tokens = [token.value.strip() for token in lexer.lex(self.condition_string)]
+        print(self.tokens)
+        return self.tokens
 
-        for i, fragment in enumerate(data):
-            token = None
-            for f_name in ALLOWED_FUNCTIONS:
-                if f_name in fragment:
-                    arg = self._get_arg(f_name, fragment)
-                    token = self.add_token(f_name, fragment, arg)
-                    if f_name == 'fctg':
-                        token = self._validate_fctg(token, data.pop(i + 1), data.pop(i + 1))
-                    break
-            if token is None:
-                continue
 
-            if i > 0 and 'not' in data[i - 1]:
-                token = f'not {token}'
-
-            tokens.append(token)
-        self.tokens = tokens
-        return tokens
-
-    def _validate_fctg(self, token: str, operator: str, value: str) -> str:
-        """
-        Проверяет валидность оператора и значения для функции fctg.
-        :param token: fctg по дефолту
-        :param operator: оператор из функции условия вызова/продления
-        :param value: значение из функции условия вызова/продления
-        :return: Токен(функция). Пример: 'fctg(G1) >= 40'
-        """
-
-        if operator not in {'>', '<', '==', '>=', '<='}:
-            raise TypeError(f'Недопустимый оператор для функции fctg: {operator}')
-        if not value.isdigit() or value.startswith('0'):
-            raise ValueError(f'Недопустимое значение функции fctg: {value}')
-        return f'{token} {operator} {value}'
-
-    def add_token(self, f_name: str, fragment: str, name_argument: str) -> str:
-        """
-        Генерирует токен(функцию)
-        :param f_name: имя функции: 'ddr', 'mr', 'fctg' и т.д.
-        :param fragment: элемент списка, из строки self.condition_string.split()
-                         '(((ddr(D1)', '(ddr(D2))', 'ddr(D3)', 'fctg >= 40' и т.д.
-        :param name_argument: Символ аргумента в зависимости от типа функции:
-                              D если ddr, G или A если mr и т.д.
-        :return: Токен(функция) вида: 'ddr(D22)', fctg(G1), 'ngp(D21)' и т.д.
-        """
-
-        patterns_regexp = {
-            'ddr': r'[0-9]{1,3}',
-            'ddo': r'[0-9]{1,3}',
-            'ngp': r'[0-9]{1,3}',
-            'mr': r'[0-9]{1,3}',
-            'dr': r'[0-9]{1,3}',
-            'fctg': r'[0-9]{1,3}'
-        }
-
-        pattern = patterns_regexp.get(f_name)
-        digit_argument = re.findall(pattern, fragment)
-        if len(digit_argument) > 1 or int(digit_argument[0]) > 128 or digit_argument[0].startswith('0'):
-            raise ValueError(f'Неверный аргумент функции {f_name}: {"".join(digit_argument)}')
-        return f'{f_name}({name_argument}{digit_argument[0]})'
-
-    def _get_arg(self, f_name: str, fragment: str) -> str:
-        """
-        Определяет часть аргумента для переданной функции.
-        :param f_name: имя токена-функции контроллера Поток: 'ddr', 'mr', 'fctg' и т.д.
-        :param fragment: Фрагмент, в котором осуществляется поиск(элемент списка, получившегося после
-                         функции split() на строке условии вызова/продления.
-                         Например: '((ddr(D25)', 'ddr(D5)', 'mr(G1)', 'and' и т.д.
-        :return: аргумент для функции: D(для функии дет), G или A для функции sg
-        """
-
-        if f_name in DET_FUNCTIONS:
-            if 'D' not in fragment:
-                raise ValueError(f'Неверный аргумент одной из фунций {f_name}')
-            arg = 'D'
-        elif f_name in SG_FUNCTIONS:
-            if 'G' in fragment:
-                arg = 'G'
-            elif 'A' in fragment:
-                arg = 'A'
-            else:
-                raise ValueError(f'Неверный аргумент одной из фунций {f_name}')
-        else:
-            raise TypeError(f'Передана некорректная функция {f_name}')
-        return arg
-
-    @classmethod
-    def get_condition_string_with_vals_instead_func(cls, string: str) -> str:
-        """
-        Заменяет "or" и "and" в строке на "+" и "*" соответственно.
-        :param string: строка, в которой требуется заменить символы.
-        :return: строка с заменёнными символами.
-        """
-
-        return cls.replace_chars(replace_data={'and': '*', 'or': '+'}, string=string)
-
-    @classmethod
-    def replace_chars(cls, replace_data: Dict[str, str], string: str):
-        """
-        Заменяет символы в строке.
-        :param string: строка, в которой требуется заменить символы.
-        :param replace_data: k: символы, которые требуется заемить, v: символы, на которые требуется заемить
-        :return: строка с заменёнными символами.
-        """
-
-        for pattern, replacement in replace_data.items():
-            string = string.replace(pattern, replacement)
-        return string
--- toolkit/sdp_lib/potok_controller/test_module.py
+++ toolkit/sdp_lib/potok_controller/test_module.py
@@ -2,38 +2,40 @@ import re
 import logging
 import functools
 import time
+from rply import LexerGenerator
 from typing import Dict
 
-string = "ddr(D4) or ddr(D5) or ddr(D6) or ddr(D7) and mr(G1)" * 10
-d1 = {'or': '+', 'and': '*'}
+
+
+
+
+lg2 = LexerGenerator()
+
+lg2.add("ddr", r'ddr\(D\d{1,3}\)')
+lg2.add("ddo", r'ddo\(D\d{1,3}\)')
+lg2.add("ngp", r'ngp\(D\d{1,3}\)')
+# lg2.add("fctg", r'fctg\(G\d+\)\s+([<>]=?|==)(\d+)')
+# lg2.add("fctg", r'fctg\(G\d+\)([<>]=?|==)\s+(\d+)')
+# lg2.add("fctg", r'fctg\(G\d+\)\s+([<>]=?|==)\s+(\d+)')
+# lg2.add("fctg", r'fctg\(G\d+\)([<>]=?|==)(\d+)')
+lg2.add("fctg", r'fctg\(G\d+\)\s*([<>]=?|==)\s*(\d+)')
+lg2.add('mr', r'\d+')
+
+# lg2.ignore(r'[\s+\(\)]')
+lg2.ignore(r'not|or|and|[\s+\(\)]')
+
+string = "(ddr(D134) or ddr(D135) or ddr(D136) or not ddr(D137)) and (    fctg(G20) < 30)"
+l = lg2.build()
+for token in l.lex(string):
+    print(token)
+    print(token.value)
+
+print([token.value.strip() for token in l.lex(string)])
+
 start_time = time.time()
 
-string2 = ''.join((re.sub(c, d1[c], string) for c in d1))
-# print(string2)
+
 
 print(f'время составило: {time.time() - start_time}')
 
 
-def replace_chars(data: Dict[str, str], string=None) -> str:
-    """
-    Заменяет символы в строке.
-    :param string: строка, в которой требуется заменить символы. если None,
-                   то берёт строку self.condition_string_vals_instead_func.
-    :param data: k: символы, которые требуется заемить, v: символы, на которые требуется заемить
-    :return: строка с заменёнными символами. если kwargs пустой, возвращает переданную строку
-    """
-
-    if not data:
-        return string
-    for pattern, replacement in data.items():
-        string = string.replace(pattern, replacement)
-    return string
-
-start_time = time.time()
-string3 = replace_chars(d1, string)
-print(f'время2 составило: {time.time() - start_time}')
-
-print(string3 == string2)
-
-print(string2)
-print(string3)
