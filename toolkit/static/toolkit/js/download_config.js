bba013638e022e4ce968f8cf0acf58628bd94447
unnmd88 <unnmd88@gmail.com>
2024-12-21 12:35:19 +0300

    Добавлен функционал загрузки конфига swarco для "Задать параметры вручную". Теперь загружать конфиг можно как по паметрам, полученным из БД, так и по параметрам, введённым вручную в соответствующее поле

--- toolkit/static/toolkit/js/download_config.js
+++ toolkit/static/toolkit/js/download_config.js
@@ -27,6 +27,18 @@ const ROOT_ROUTE_API = '/api/v1/'
 const ROOT_ROUTE_API_GET_TRAFFIC_LIGHT_OBJECT = '/api/v1/trafficlight-objects/'
 const ROUTE_API_DOWNLOAD_CONFIG = `${ROOT_ROUTE_API}download-config/`;
 
+const inputSearchInDb = document.querySelector('#search_controller');
+const radioSearchParameter = document.querySelectorAll('.search_parameter');
+const radioSearchIndb = document.querySelector("#by_search_in_db");
+const radioSearchIpAddress = document.querySelector("#by_ipAddr");
+
+const inputIpAddress = document.querySelector('#ip_controller');
+const selectChooseControllerType = document.querySelector('#type_controller_from_user');
+
+const btnDownloadConfig = document.querySelector('#get_config');
+
+
+ 
 
 
 
@@ -116,7 +128,6 @@ async function set_request_axios(num_host) {
 --------------------------------------------------------------------------*/
 
 // Запись в "дисплей" полученного состояния ДК(get request)
-
 function write_data_to_display_get(display, type, data) {
     // Шапка, общая для всех "дисплеев"
     display.textContent += `\n\nВремя запроса: ${data.request_time}`;
@@ -165,10 +176,12 @@ function createContentPeekGet_Display(data_resp_json) {
 --------------------------------------------------------------------------*/
 
 
+inputSearchInDb.addEventListener('input', search_host_get_data);
+radioSearchParameter.forEach((el) => {
+  el.addEventListener('change', radioChange);
+});
+inputIpAddress.addEventListener('input', checkValidDataFromUser);
 
-
-const search_input = document.querySelector('#search_controller');
-search_input.addEventListener('input', search_host_get_data);
 
 async function search_host_get_data(event) {
 
@@ -221,7 +234,7 @@ function write_data_about_host(data) {
     const content_elements = {
         ip_adress: document.querySelector('#ip_addr_val'),
         type_controller:document.querySelector('#type_controller_val'),
-        adress: document.querySelector('#address_val'),
+        address: document.querySelector('#address_val'),
         description: document.querySelector('#description_val')
     }
 
@@ -245,19 +258,24 @@ function check_valid_data_to_send_req(data) {
     }
 }
 
-const btnDownloadConfig = document.querySelector('#get_config');
+
 btnDownloadConfig.addEventListener('click', download_config);
 
 // Функция отправки зароса на загрузку конфига
 async function download_config(event) {
   console.log('download_configППП');
   document.querySelector('#search_controller').value
+  const src = document.querySelector('input[type="radio"][name="search_option"]:checked');
+  console.log(src);
+
+  waitingEffect(true);
+
   try {
       const response = await axios.post(            
         ROUTE_API_DOWNLOAD_CONFIG,
         {
           // hosts: {[document.querySelector('#ip_addr_val').textContent]: {"host_id": "", "type_controller": "Swarco"}},
-          hosts: collectDataForRequset(),
+          hosts: collectDataForRequset(src.id),
           type_request: 'get_config'
         },
           {
@@ -304,21 +322,24 @@ async function download_config(event) {
       }
       console.log(error.config);
     }
+    finally {
+      waitingEffect(false);
+    }
   
 }
 
-function collectDataForRequset(type_data='fromDB') {
+function collectDataForRequset(type_data='by_search_in_db') {
 
   const hosts = {};
   let ipAddr, type_controller, host_id, address;
-  if (type_data === 'fromDB') {
+  if (type_data === 'by_search_in_db') {
     ipAddr = document.querySelector('#ip_addr_val').textContent;
     type_controller = document.querySelector('#type_controller_val').textContent;
     address = document.querySelector('#address_val').textContent;
     host_id = document.querySelector('#search_controller').value;
 
   }
-  else if (type_data === 'fromUser') {
+  else if (type_data === 'by_ipAddr') {
     ipAddr = document.querySelector('#ip_controller').value;
     const select_type_controller = document.querySelector('#type_controller_from_user');
     type_controller = select_type_controller.options[select_type_controller.selectedIndex].text;
@@ -335,5 +356,49 @@ function collectDataForRequset(type_data='fromDB') {
   return hosts;
 }
 
+function radioChange() {
+  console.log('radioSearchIndb.checked: '+ radioSearchIndb.checked)
+  if (radioSearchIndb.checked) {
+      inputIpAddress.disabled = true;
+      selectChooseControllerType.disabled = true;
+      inputIpAddress.value = '';
+      inputSearchInDb.disabled = false;
+  }
+  else {
+    document.querySelectorAll('.search_result_content').forEach((el) => {
+      el.textContent = '';
+    });
+      inputIpAddress.disabled = false;
+      selectChooseControllerType.disabled = false;
+      inputSearchInDb.disabled = true;
+      inputSearchInDb.value = '';
+  }
+}
 
+function checkValidDataFromUser() {
+  if (ipV4IsValid(inputIpAddress.value) && selectChooseControllerType.value == CONTROLLERS[0]) {
+    btnDownloadConfig.disabled = false;
+  }
+  else {
+    btnDownloadConfig.disabled = true;
+  }
+}
 
+function ipV4IsValid(ip) {
+  const ipv4Pattern = /^(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})(\.(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})){3}$/;
+  if (ipv4Pattern.test(ip)) {
+    return true;
+  }
+  return false;
+}
+
+function waitingEffect(state) {
+  if (state) {
+    btnDownloadConfig.textContent = 'Ожидайте, идет процесс загрузки'
+    btnDownloadConfig.disabled = true;
+  }
+  else {
+    btnDownloadConfig.textContent = 'Отправить запрос'
+    btnDownloadConfig.disabled = false;
+  }
+}
