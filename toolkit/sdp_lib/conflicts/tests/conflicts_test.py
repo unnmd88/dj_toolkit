770effbafdee24ecc968eb9508ea8ef92189c5e9
unnmd88_h <unnmd88@gmail.com>
2024-12-25 18:31:41 +0300

    Добавлены тесты для модуля conflicts

--- toolkit/sdp_lib/conflicts/tests/conflicts_test.py
+++ toolkit/sdp_lib/conflicts/tests/conflicts_test.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 from toolkit.sdp_lib.conflicts.calculate_conflicts import Conflicts, DataFields
 
+
 class TestConflicts(TestCase):
     def setUp(self) -> None:
         raw_data_stages = {
@@ -26,6 +27,26 @@ class TestConflicts(TestCase):
         self.assertEqual(
             self.current_calculation.instance_data[DataFields.sorted_stages_data.value]['2'],
             [1, 3, 6, 7]
+        )
+
+    def test_num_group_and_num_stages(self):
+        """
+        Проверяет корректность определения количества направлений и количества фаз
+        :return:
+        """
+        self.assertEqual(self.current_calculation.instance_data[DataFields.number_of_groups.value], 13)
+        self.assertEqual(self.current_calculation.instance_data[DataFields.number_of_stages.value], 4)
+        self.assertEqual(
+            self.current_calculation.instance_data[DataFields.all_num_groups.value], [i for i in range(1, 14)]
+        )
+
+    def test_define_allow_make_config(self):
+        """
+        Проверят корректность определения ключа "allow_make_config" из словаря self.instance_data.
+        :return:
+        """
+        self.assertTrue(
+            self.current_calculation.instance_data[DataFields.allow_make_config.value], True
         )
 
 
