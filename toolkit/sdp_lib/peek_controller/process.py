79906796c0483d4d73e317cddf256a0348f705fa
unnmd88_h <unnmd88@gmail.com>
2025-01-24 01:57:05 +0300

    Завершена первая реализация модуля process.py пакета peek_controller

--- toolkit/sdp_lib/peek_controller/process.py
+++ toolkit/sdp_lib/peek_controller/process.py
@@ -1,40 +1,137 @@
-import functools
-import operator
-from typing import List, Tuple
+import json
+from dataclasses import dataclass, field
+from enum import StrEnum
+from typing import List
 
-from toolkit.tmp_py_files.cmd_sg.calculate import CmdSg
+
+class CmdSg(StrEnum):
+    GREEN = '3'
+    RED = '1'
+    DISABLED = '0'
 
 
+# class Intersection:
+#     def __init__(self, source_xp_data: Dict[str, Tuple], identifier: str = '-- Intersection --'):
+#         self.source_xp_data = dict(sorted(source_xp_data.items()))
+#         self.repaired_xp_data: Dict[str, Tuple] = {}
+#         self.identifier = identifier
+#         self.num_stages = 0
+#
+#     def __repr__(self):
+#         return self.get_pretty_output()
+#
+#     def get_pretty_output(self):
+#
+#         pretty_output = f''
+#         self.num_stages = 1
+#         for xp, xp_data in self.repaired_xp_data.items():
+#             pretty_cmd_sg = self.make_pretty_cmd_sg(xp_data[1])
+#             curr_xp = (
+#                 f'Process: {xp}\n'
+#                 f'Groups in process: {xp_data[0]}\n'
+#                 f'Groups in CmdSG repaired:\n{pretty_cmd_sg}'
+#             )
+#             pretty_output += curr_xp
+#         return f'{self.identifier}\nNumber of stages: {self.num_stages - 1}\n{pretty_output}'
+#
+#     def make_pretty_cmd_sg(self, cmd_sg: List[str]) -> str:
+#         """
+#         Формирует строки CmdSG из "Process" EC-X Configurator для удобного вывода/записи в файл.
+#         :param curr_stage:
+#         :param cmd_sg: Список строк CmdSG.
+#         :return: Удобочитаемая строка для вывода/записи в файл.
+#         """
+#         if cmd_sg is not None:
+#             _data = ''
+#             for cmd_sg_stage in cmd_sg:
+#                 _data += f'Stage{" " * (2 if self.num_stages < 10 else 1)}{self.num_stages}: {cmd_sg_stage}\n'
+#                 self.num_stages += 1
+#             return _data
+#
+#     def repair_cmd_sg_all_stages(self):
+#         """
+#         Заменяет значение группы в строке процесса CmdSG на 0, если группа не участвует в процессе
+#         для всех фаз процесса.
+#         :return: Список строк CmdSG, где индекс + 1 -> номер фазы
+#         """
+#
+#         for xp, xp_data in self.source_xp_data.items():
+#             stage_in_curr_xp, cmd_sg_in_curr_xp = list(map(str, sorted(xp_data[0]))), xp_data[1]
+#             self.repaired_xp_data[xp] = (
+#                 ",".join(stage_in_curr_xp), self._repair_line_stage(stage_in_curr_xp, cmd_sg_in_curr_xp)
+#             )
+#
+#     def _repair_line_stage(self, stages_in_process: List[str], cmd_sg_all_stages_in_process: List[str]) -> List[str]:
+#         """
+#         Заменяет значение группы в строке line_stage на 0, если группа не участвует в процессе.
+#         :param line_stage: строка CmdSG из "Process" EC-X Configurator
+#         :return: скорректированная строка CmdSG, которую можно скопировать в "Process" EC-X Configurator
+#         """
+#
+#         repaired_cmd_sg_all_stages = []
+#         for cmd_sg_stage in cmd_sg_all_stages_in_process:
+#             repaired_curr_stage_cmd_sg = ",".join([
+#                 val if str(num_group) in stages_in_process else CmdSg.DISABLED.value
+#                 for num_group, val in enumerate(cmd_sg_stage.split(','), 1)
+#             ])
+#             repaired_cmd_sg_all_stages.append(repaired_curr_stage_cmd_sg)
+#         return repaired_cmd_sg_all_stages
+#
+#     def write(
+#             self, data_for_write: List[str] | str | None = None,
+#             filename: str = 'repaired_CmdSG.txt',
+#             mode: str = 'w'
+#     ) -> None:
+#         data_for_write = data_for_write or self.get_pretty_output()
+#         with open(filename, mode) as f:
+#             if isinstance(data_for_write, str):
+#                 f.write(data_for_write)
+#             elif isinstance(data_for_write, list):
+#                 for line in data_for_write:
+#                     f.write(f'{line}\n')
+#             else:
+#                 raise TypeError('Данные для записи в файл должны быть строкой или списком')
+
+@dataclass
 class Intersection:
+    source_xp_data: dict[str, tuple[str]]
+    identifier: str = '-- Intersection --'
+    num_stages: int = 0
+    repaired_xp_data: dict[str, tuple[str, list[str]]] = field(default_factory=dict)
 
-    def __init__(self, xp, groups_in_xp, source_xp_cmd_sg):
-        self.xp = xp
-        self.groups_in_xp = set(map(str, groups_in_xp))
-        self.source_cmd_sg = source_xp_cmd_sg
-        self.repaired_cmd_sg = None
+    def __post_init__(self):
+        self.source_xp_data = dict(sorted(self.source_xp_data.items()))
 
     def __repr__(self):
-        return (f'Process: {self.xp}\n'
-                f'Groups in process: {", ".join(list(map(str, sorted(map(int, self.groups_in_xp)))))}\n'
-                f'Groups in CmdSG source:\n{self.make_pretty_cmd_sg(self.source_cmd_sg)}'
-                f'Groups in CmdSG repaired:\n{self.make_pretty_cmd_sg(self.repaired_cmd_sg)}')
+        return self.get_pretty_output()
 
-    def __add__(self, other):
-        if isinstance(other, Intersection):
-            return f'{repr(self)}\n{repr(other)}'
-        return NotImplemented
+    def get_pretty_output(self):
 
-    def make_pretty_cmd_sg(self, cmd_sg: List[str]) -> str:
+        pretty_output = f''
+        self.num_stages = 1
+        for xp, xp_data in self.repaired_xp_data.items():
+            pretty_cmd_sg = self.make_pretty_cmd_sg(xp_data[1])
+            curr_xp = (
+                f'Process: {xp}\n'
+                f'Groups in process: {xp_data[0]}\n'
+                f'Groups in CmdSG repaired:\n{pretty_cmd_sg}'
+            )
+            pretty_output += curr_xp
+        return f'{self.identifier}\nNumber of stages: {self.num_stages - 1}\n{pretty_output}'
+
+    def make_pretty_cmd_sg(self, cmd_sg: list[str]) -> str:
         """
         Формирует строки CmdSG из "Process" EC-X Configurator для удобного вывода/записи в файл.
+        :param curr_stage:
         :param cmd_sg: Список строк CmdSG.
         :return: Удобочитаемая строка для вывода/записи в файл.
         """
         if cmd_sg is not None:
-            data = ''
-            for num_stage, cmd_sg_stage in enumerate(cmd_sg, 1):
-                data += f'Stage{" " * (2 if num_stage < 10 else 1)}{num_stage}: {cmd_sg_stage}' + '\n'
-            return data
+            _data = ''
+            for cmd_sg_stage in cmd_sg:
+                _data += f'Stage{" " * (2 if self.num_stages < 10 else 1)}{self.num_stages}: {cmd_sg_stage}\n'
+                self.num_stages += 1
+            return _data
 
     def repair_cmd_sg_all_stages(self):
         """
@@ -42,72 +139,111 @@ class Intersection:
         для всех фаз процесса.
         :return: Список строк CmdSG, где индекс + 1 -> номер фазы
         """
-        self.repaired_cmd_sg = [self._repair_line_stage(line_stage) for line_stage in self.source_cmd_sg]
-        return self.repaired_cmd_sg
 
-    def _repair_line_stage(self, line_stage: str):
+        for xp, xp_data in self.source_xp_data.items():
+            stage_in_curr_xp, cmd_sg_in_curr_xp = list(map(str, sorted(xp_data[0]))), xp_data[1]
+            self.repaired_xp_data[xp] = (
+                ",".join(stage_in_curr_xp), self._repair_line_stage(stage_in_curr_xp, cmd_sg_in_curr_xp)
+            )
+
+    def _repair_line_stage(self, stages_in_process: List[str], cmd_sg_all_stages_in_process: List[str]) -> List[str]:
         """
         Заменяет значение группы в строке line_stage на 0, если группа не участвует в процессе.
         :param line_stage: строка CmdSG из "Process" EC-X Configurator
         :return: скорректированная строка CmdSG, которую можно скопировать в "Process" EC-X Configurator
         """
-        return ",".join([
-            val if str(num_group) in self.groups_in_xp else CmdSg.DISABLED.value
-            for num_group, val in enumerate(line_stage.split(','), 1)
-        ])
 
+        repaired_cmd_sg_all_stages = []
+        for cmd_sg_stage in cmd_sg_all_stages_in_process:
+            repaired_curr_stage_cmd_sg = ",".join([
+                val if str(num_group) in stages_in_process else CmdSg.DISABLED.value
+                for num_group, val in enumerate(cmd_sg_stage.split(','), 1)
+            ])
+            repaired_cmd_sg_all_stages.append(repaired_curr_stage_cmd_sg)
+        return repaired_cmd_sg_all_stages
 
-    def read_cmd_sg(self):
-        with open('CO413.DAT') as f:
-            acc = []
-            for line in f:
+    def write(
+            self, data_for_write: List[str] | str | None = None,
+            filename: str = 'repaired_CmdSG.txt',
+            mode: str = 'w'
+    ) -> None:
+        data_for_write = data_for_write or self.get_pretty_output()
+        with open(filename, mode) as f:
+            if isinstance(data_for_write, str):
+                f.write(data_for_write)
+            elif isinstance(data_for_write, list):
+                for line in data_for_write:
+                    f.write(f'{line}\n')
+            else:
+                raise TypeError('Данные для записи в файл должны быть строкой или списком')
 
-                if 'CmdSG' in line and len(line) > 20:
-                    acc.append(line)
-        print(*acc)
-        print(len(acc))
+if __name__ == '__main__':
+    groups_in_xp1 = {1, 4, 20, 22, 23, 29, 30, 32, 34, 35, 42, 44, 45, 46, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+                     60}
+    xp1_cmd_sg = [
+        '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1',
+        '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1',
+        '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1',
+        '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1',
+        '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,3,1,1,1,1,1,1,1',
+        '1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,3,3,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1',
+        '1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1',
+        '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1',
+        '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1',
+        '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1',
+        '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1',
+        '1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3',
+    ]
+    groups_in_xp2 = {2, 12, 13, 14, 15, 31, 33, 41}
+    xp2_cmd_sg = [
+        (
+            '1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
+        (
+            '1,3,1,1,1,1,1,1,1,1,1,3,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
+        (
+            '1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
+        (
+            '1,3,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
+        (
+            '1,3,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
+    ]
+    groups_in_xp3 = {9, 27, 37}
+    xp3_cmd_sg = [
+        '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+    ]
+    groups_in_xp4 = {3, 5, 6, 7, 8, 10, 11, 16, 17, 18, 19, 21, 24, 25, 26, 28, 36, 38, 39, 40, 43, 47, 48}
+    xp4_cmd_sg = [
+        '1,1,3,1,1,1,3,3,1,3,3,1,1,1,1,3,1,1,1,1,3,1,1,1,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,3,1,1,1,3,3,1,1,3,1,1,1,1,3,3,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,3,1,3,1,1,1,1,1,3,1,1,1,1,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,1,3,3,3,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,3,3,1,1,1,3,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,3,3,3,1,1,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,3,1,1,1,1,1,1,3,3,1,1,1,1,3,1,1,1,1,3,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,3,1,1,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,3,3,1,1,1,3,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,3,3,3,1,1,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,3,1,1,1,3,3,1,3,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,1,3,3,3,1,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,1,3,3,3,1,1,3,1,1,1,1,1,3,3,1,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+    ]
 
-    # def write(self):
-    #     with open('413_stages_new.txt', 'w') as f:
-    #         f.write(f'xp {self.xp}: \n'
-    #                 f'Группы: {",".join(list(map(str, sorted(map(int, self.groups_in_xp)))))}\n')
-    #         for num_stage, groups in self.calculated_stages_and_groups.items():
-    #             cmdSG = self.get_cmdSG_line(self.groups_in_xp, groups)
-    #             f.write(f'stage {num_stage}:\n'
-    #                     f'{groups}\n'
-    #                     f'{",".join(cmdSG)}\n{"-" * 40}\n')
-    #         f.write(f'\n')
+    all_intersections = {
+        '1': (groups_in_xp1, xp1_cmd_sg),
+        '2': (groups_in_xp2, xp2_cmd_sg),
+        '3': (groups_in_xp3, xp3_cmd_sg),
+        '4': (groups_in_xp4, xp4_cmd_sg),
+    }
 
+    data = Intersection(all_intersections, 'СО 413 Тверская застава')
+    data.repair_cmd_sg_all_stages()
+    print(json.dumps(data.repaired_xp_data, indent=4))
+    print(data)
+    data.write()
+    data.write(json.dumps(data.repaired_xp_data, indent=4), filename='data.json')
 
-groups_in_xp1 = {1,4,20,22,23,29,30,32,34,35,42,44,45,46,49,50,51,52,53,54,55,56,57,58,59,60}
-xp1_cmd_sg = [
-    '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1',
-    '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1',
-    '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1',
-    '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1',
-    '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,3,1,1,1,1,1,1,1',
-    '1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,3,1,3,3,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1',
-    '1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1',
-    '3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1',
-    '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1',
-    '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,3,3,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1',
-    '3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1',
-    '1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3',
-]
-
-groups_in_xp2 = {2,12,13,14,15,31,33,41}
-xp2_cmd_sg = [
-    ('1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
-    ('1,3,1,1,1,1,1,1,1,1,1,3,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
-    ('1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
-    ('1,3,1,1,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
-    ('1,3,1,1,1,1,1,1,1,1,1,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1'),
-]
-
-xp1 = Intersection(xp=1, groups_in_xp=groups_in_xp1, source_xp_cmd_sg=xp1_cmd_sg)
-xp1.repair_cmd_sg_all_stages()
-
-xp2 = Intersection(xp=2, groups_in_xp=groups_in_xp2, source_xp_cmd_sg=xp2_cmd_sg)
-xp2.repair_cmd_sg_all_stages()
-
-print(xp2 + xp1)
+    #
+    # calculated_intersections = calculate(all_intersections)
+    # res = UnionIntersections(calculated_intersections)
+    # print(res)
