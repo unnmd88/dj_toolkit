c3d7b05a73ed29cadadba095bbfb0ae801e707d5
deb12_h <unnmd88@gmail.com>
2025-01-28 19:02:20 +0300

    Подготовка к реализации обновленного controller_management

--- toolkit/static/toolkit/js/controller_management_v2.js
+++ toolkit/static/toolkit/js/controller_management_v2.js
@@ -48,12 +48,13 @@ const SEARCH_OPTIONS = ['По номеру СО', 'Названию СО'];
 //home linux mint
 //  const TOKEN = '52b115bf712aa113b2cd16c69e0e1e774158feb3'
 // home linux deb
+const TOKEN = '5f2c92774d1c1e0795335dd86fadc39b661c65f1';
 //home
 // const TOKEN = '7174fa6f9d0f954a92d2a5852a7fc3bcaace7578';
 //work
 // const TOKEN = '5f2c92774d1c1e0795335dd86fadc39b661c65f1';
 
-const TOKEN = 'a090474ab50a6ec440eef021295d5f0e750afa00';
+// const TOKEN = 'a090474ab50a6ec440eef021295d5f0e750afa00';
 // const TOKEN = 'fb682e5942fa8ce5c26ab8cd3e8eaba41c4cd961'; shared_desktop
 const GET_DATA = 'get_data';
 const SET_DATA = 'set_data';
